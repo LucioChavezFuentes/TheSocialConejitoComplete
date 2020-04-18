88530d3ff4eb00dbbb6fea29e7a80a38bfe2d775
Lucio Chavez <luciobertinchavez@gmail.com>
2020-04-18 14:14:23 -0500

    Remove minor unused variables

--- packages/theSocialConejito-client/src/redux/actions/uiActions.tsx
+++ packages/theSocialConejito-client/src/redux/actions/uiActions.tsx
@@ -1,8 +1,6 @@
 
-import {CLEAR_ERRORS, CLOSE_WINDOW_POST_SCREAM, OPEN_WINDOW_POST_SCREAM, CLOSE_DELETE_SCREAM_ALERT, OPEN_DELETE_SCREAM_ALERT, CANCEL_SET_SCREAM} from '../types/actionTypes/uiTypes';
+import {CLEAR_ERRORS, CLOSE_WINDOW_POST_SCREAM, OPEN_WINDOW_POST_SCREAM, CLOSE_DELETE_SCREAM_ALERT, OPEN_DELETE_SCREAM_ALERT} from '../types/actionTypes/uiTypes';
 import {Dispatch} from '../types';
-import { CancelTokenSource } from 'axios';
-
 
 //Clear Errors
 export const clearErrors = () => (dispatch: Dispatch) => {
