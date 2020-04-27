d99bb196de6fd5d7fb7a3fee253e97016b39025d
Lucio Chavez <luciobertinchavez@gmail.com>
2020-04-27 14:19:11 -0500

    clean up variables

--- packages/theSocialConejito-client/src/pages/__tests__/home.test.tsx
+++ packages/theSocialConejito-client/src/pages/__tests__/home.test.tsx
@@ -1,11 +1,9 @@
-//jest.mock('../../redux/actions/dataActions')
 
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import Home from '../home';
 import { Provider } from 'react-redux';
 import store from '../../redux/store';
-import {normalize} from 'normalizr';
 import { mocked } from 'ts-jest/utils';
 import axios from 'axios';
 //MUI Imports
@@ -13,18 +11,11 @@ import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 
 jest.mock('axios');
 
-import * as schema from '../../redux/schema';
-
-//import dataActions
-//import * as dataActions from '../../redux/actions/dataActions'
-//import dataTypes
-import * as dataTypes from '../../redux/types/actionTypes/dataTypes';
 //React Testing Library
-import { render, fireEvent, cleanup, waitFor, waitForDomChange, wait, screen } from '@testing-library/react';
+import { render, cleanup, waitFor} from '@testing-library/react';
 
 //the library to create snapshos of components
 import renderer from 'react-test-renderer';
-import { Dispatch } from 'redux';
 
 const theme = createMuiTheme({
 
@@ -38,15 +29,6 @@ const theme = createMuiTheme({
     },
   })
 
-  afterEach(() => {
-    //@ts-ignore
-    axios.mockClear()
-  })
-
-//const mockGetScreams = mocked(dataActions.getScreams, true)
-
-afterEach(cleanup);
-
 const HomeComponent = () => (
     <MuiThemeProvider theme={theme}>
         <Provider store={store}>
@@ -59,8 +41,15 @@ const HomeComponent = () => (
 
 
 describe('home page', () => {
-    
 
+  //const mockGet = mocked(axios.get, true)
+  const mockAxios = mocked(axios, true);
+    
+  beforeEach(() => {
+    mockAxios.mockClear()
+  });
+
+  afterEach(cleanup);
     
 
     test("Home component display screams correctly", async () => {
@@ -74,18 +63,16 @@ describe('home page', () => {
             }
         
         //const normalizedScreams : dataTypes.ScreamSchema = normalize(payload.data, schema.arrayOfScreams)
-
-        axios.get.mockResolvedValue(payload);
+        
+        mockAxios.get.mockResolvedValue(payload);
         /*dataActions.getScreams.mockImplementation(() =>  {
 
             return {type:'SET_SCREAMS', payload: normalizedScreams }
         
         })*/
-         render(<HomeComponent />)
-        //wfawexpect(screen.getByText('Buenas monos')).toBeTruthy()
-        await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1))
-        await waitFor(() => expect(screen.getByText('Buenas monos')).toBeTruthy())
-
+        const {getByText} = render(<HomeComponent />);
+        await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+        await waitFor(() => expect(getByText('Buenas monos')).toBeTruthy());
         
     });
 
